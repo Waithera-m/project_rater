c6e43c4b540e15c641a6fe723fd284b2b8369652
Waithera <njihiamary11@gmail.com>
2024-09-19 22:15:43 +0300

    update django version

--- project_rater/settings.py
+++ project_rater/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import sys
 from decouple import config
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,4 +156,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
